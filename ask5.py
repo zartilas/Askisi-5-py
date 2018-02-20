35422b225c0a055cb2c9f8f01b0fd1319d03ac20
zartilas <zartilasvasilios98@gmail.com>
2018-02-20 20:56:48 +0200

    first commit

---
+++ ask5.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#Ασκηση 5
+#ράψτε ένα πρόγραμμα σε Python το οποίο παίρνει σαν είσοδο από το χρήστη
+#το προφίλ ενός χρήστη στο Twitter και στη συνέχεια εμφανίζει τη δημοφιλέστερη λέξη των 10 τελευταίων tweets
+
+import tweepy
+from tweepy import OAuthHandler
+
+#συνδεομαστε με την εφαρμογή tweepy
+pip install tweepy==3.3.0
